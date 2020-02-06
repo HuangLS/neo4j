4b86782f2c506bc34edbc3c6669d8d67864f6c48
rayz <irayz@outlook.com>
2020-02-06 11:23:46 +0800

    add temporalrangequery interface in neo4j

--- community/kernel/src/main/java/org/neo4j/temporal/TemporalRangeQuery.java
+++ community/kernel/src/main/java/org/neo4j/temporal/TemporalRangeQuery.java
@@ -1,13 +1,44 @@
 package org.neo4j.temporal;
 
-import org.act.temporalProperty.query.range.InternalEntryRangeQueryCallBack;
+import org.act.temporalProperty.impl.InternalEntry;
+import org.act.temporalProperty.impl.InternalKey;
+import org.act.temporalProperty.impl.ValueType;
+import org.act.temporalProperty.query.TimePointL;
 import org.act.temporalProperty.query.range.TimeRangeQuery;
+import org.act.temporalProperty.util.Slice;
 
 /**
  * Created by song on 2018-07-26.
  */
 
-public interface TemporalRangeQuery extends TimeRangeQuery
+public abstract class TemporalRangeQuery implements TimeRangeQuery
 {
+    private CallBack callBack = null;
+    @Override
+    public void setValueType(String valueType) {
+        switch (valueType){
+            case "INT": this.callBack = new CallBack(){ @Override public Object readSlice(Slice slice) { return slice.getInt(0); }}; return;
+            case "LONG": this.callBack = new CallBack(){ @Override public Object readSlice(Slice slice) { return slice.getLong(0); }}; return;
+            case "FLOAT": this.callBack = new CallBack(){ @Override public Object readSlice(Slice slice) { return slice.getFloat(0); }}; return;
+            case "DOUBLE": this.callBack = new CallBack(){ @Override public Object readSlice(Slice slice) { return slice.getDouble(0); }}; return;
+            case "STRING": this.callBack = new CallBack(){ @Override public Object readSlice(Slice slice) { return new String(slice.getBytes()); }}; return;
+        }
+    }
 
+    @Override
+    public void onNewEntry(InternalEntry entry) {
+        InternalKey k = entry.getKey();
+        Slice v = entry.getValue();
+        if(k.getValueType()==ValueType.INVALID){
+            onNewEntry(k.getEntityId(), k.getPropertyId(), k.getStartTime(), null);
+        }else{
+            onNewEntry(k.getEntityId(), k.getPropertyId(), k.getStartTime(), callBack.readSlice(v));
+        }
+    }
+
+    private interface CallBack{
+        Object readSlice(Slice slice);
+    }
+
+    public abstract void onNewEntry(long entityId, int propertyId, TimePointL time, Object val);
 }
